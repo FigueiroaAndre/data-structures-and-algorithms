95abf4cf95bdd5dd4f47099a5727a73508df8d02
André Figueirôa <alfigueiroab@gmail.com>
2021-08-17 08:55:19 -0300

    Update priorityQueue example

--- src/data-structures/priority-queue/example.ts
+++ src/data-structures/priority-queue/example.ts
@@ -1,36 +1,26 @@
 import PriorityQueue from "./PriorityQueue";
 
-let pq = new PriorityQueue<number>();
-let input = [4,8,14,1,3,22];
+let input = [4,8,14,1,3,22,5,2,9];
+let minPQ = new PriorityQueue<number>(input);
+let maxPQ = new PriorityQueue<number>(input, (v1,v2) => v1 > v2);
 
-input.forEach(value => {
-    pq.add(value);
-    console.log(value + ' was added!!');
-    pq.out();
-});
-
-let removedValue = pq.poll();
-console.log(removedValue + ' was removed!!');
-
-pq.add(2);
-console.log('2 was added!!');
-
-removedValue = pq.poll();
-console.log(removedValue + ' was removed!!');
-
-pq.add(4);
-console.log('4 was added!!');
-
-removedValue = pq.poll();
-console.log(removedValue + ' was removed!!');
-
-pq.add(5);
-console.log('5 was added!!');
-
-pq.add(9);
-console.log('9 was added!!');
-
-while(pq.size() > 0) {
-    removedValue = pq.poll();
-    console.log(removedValue + ' was removed!!');
+while (minPQ.size() !== 0) {
+    let removedValue = minPQ.poll();
+    console.log('[MIN-PQ]: ' + removedValue + ' was polled!!');
 }
+console.log('===')
+
+while (maxPQ.size() !== 0) {
+    let removedValue = maxPQ.poll();
+    console.log('[MAX-PQ]: ' + removedValue + ' was polled!!');
+}
+console.log('===');
+
+console.log('[MIN-PQ]: refiling with these values: ' + input);
+input.forEach(value => minPQ.add(value));
+console.log('[MIN-PQ]: current state of internal heap: ');
+minPQ.out();
+minPQ.remove(5);
+console.log('[MIN-PQ]: 5 was removed!!');
+console.log('[MIN-PQ]: current state of internal heap: ');
+minPQ.out();
